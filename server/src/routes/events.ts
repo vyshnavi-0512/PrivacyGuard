aa6f4bac4ad06a54fa2f6cfbbc8d4b1ac8e93a5d
Vyshnavi <vyshnavi.madishetti5@gmail.com>
2026-06-14 15:27:04 +0530

    Add GET events route

--- server/src/routes/events.ts
+++ server/src/routes/events.ts
@@ -2,6 +2,12 @@ import { Router } from "express";
 
 const router = Router();
 
+router.get("/events", (_req, res) => {
+  res.json({
+    message: "events route reached"
+  });
+});
+
 router.post("/events", (req, res) => {
   console.log("Analytics event:", req.body);
   res.json({ success: true });
